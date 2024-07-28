13948c3cf1fd135fefecb0e76ef551df80aef362
21120453 <phuonghieuto@gmail.com>
2024-07-29 00:17:12 +0700

    implement category service, implement avatar component

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,8 @@
-import { IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faUser, IconDefinition} from "@fortawesome/free-solid-svg-icons";
 import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
 
 // Import the icons that will be used by the application
 export const fontAwesomeIcons: IconDefinition[] = [
-    faAirbnb
+    faAirbnb,
+    faUser
 ]
