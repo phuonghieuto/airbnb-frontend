3de6aef2a57cfc6cd461f6c3052f5a78808e747a
21120453 <phuonghieuto@gmail.com>
2024-07-28 23:56:47 +0700

    import icons, create components, services, models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome"
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, FaIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-frontend';
+export class AppComponent implements OnInit {
+  faIconLibrary = inject(FaIconLibrary);
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+import { IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+
+// Import the icons that will be used by the application
+export const fontAwesomeIcons: IconDefinition[] = [
+    faAirbnb
+]
