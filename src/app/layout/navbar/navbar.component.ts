f01d5117bcef34578e54b47dd7a47c162c0bb1b2
21120453 <phuonghieuto@gmail.com>
2024-08-03 20:45:59 +0700

    implement landlord's models: listing, picture

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, effect, inject, OnInit} from '@angular/core';
 import {ButtonModule} from "primeng/button";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {ToolbarModule} from "primeng/toolbar";
 import {MenuModule} from "primeng/menu";
 import {CategoryComponent} from "./category/category.component";
 import {AvatarComponent} from "./avatar/avatar.component";
-import {DialogService} from "primeng/dynamicdialog";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
 import {MenuItem} from "primeng/api";
 import {ToastService} from "../toast.service";
+import {AuthService} from "../../core/auth/auth.service";
+import {User} from "../../core/model/user.model";
 
 @Component({
   selector: 'app-navbar',
@@ -32,23 +34,81 @@ export class NavbarComponent implements OnInit {
   dates: string = "Any week";
 
   toastService = inject(ToastService);
+  dialogService = inject(DialogService);
+  authService = inject(AuthService);
+  ref: DynamicDialogRef | undefined;
+  login = () => this.authService.login();
+
+  logout = () => this.authService.logout();
 
   currentMenuItems: MenuItem[] | undefined = [];
-  ngOnInit(): void {
-    this.fetchMenu();
-    this.toastService.send({severity: 'info', summary: 'Welcome'});
+
+  connectedUser: User = {email: this.authService.notConnected};
+
+  constructor() {
+    effect(() => {
+      if (this.authService.fetchUser().status === "OK") {
+        this.connectedUser = this.authService.fetchUser().value!;
+        this.currentMenuItems = this.fetchMenu();
+      }
+    });
   }
 
-  private fetchMenu() {
-    return [
-      {
-        label: "Sign up",
-        styleClass: "font-bold"
-      },
-      {
-        label: "Login"
-      }
-    ];
+  ngOnInit(): void {
+    this.authService.fetch(false);
+  }
+
+  private fetchMenu(): MenuItem[] {
+    if (this.authService.isAuthenticated()) {
+      return [
+        {
+          label: "My properties",
+          routerLink: "landlord/properties",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: "My booking",
+          routerLink: "booking",
+        },
+        {
+          label: "My reservation",
+          routerLink: "landlord/reservation",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: "Log out",
+          command: this.logout
+        },
+      ]
+    } else {
+      return [
+        {
+          label: "Sign up",
+          styleClass: "font-bold",
+          command: this.login
+        },
+        {
+          label: "Log in",
+          command: this.login
+        }
+      ]
+    }
+  }
+
+  hasToBeLandlord(): boolean {
+    return this.authService.hasAnyAuthority("ROLE_LANDLORD");
+  }
+
+  openNewListing(): void {
+    // this.ref = this.dialogService.open(PropertiesCreateComponent,
+    //   {
+    //     width: "60%",
+    //     header: "Airbnb your home",
+    //     closable: true,
+    //     focusOnShow: true,
+    //     modal: true,
+    //     showHeader: true
+    //   })
   }
 
 }
