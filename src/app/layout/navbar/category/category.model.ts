f3cbacbc24b2bdeac33b81e4bc79fd587bcf85fb
21120453 <phuonghieuto@gmail.com>
2024-08-10 16:24:55 +0700

    fix change category, add missing category

--- src/app/layout/navbar/category/category.model.ts
+++ src/app/layout/navbar/category/category.model.ts
@@ -1,9 +1,29 @@
 import {IconName} from "@fortawesome/angular-fontawesome";
 
-export type CategoryName = "ALL" | "AMAZING_VIEWS" | "OMG" | "TREEHOUSES"
-  | "BEACH" | "FARMS" | "TINY_HOMES" | "LAKE" | "CONTAINERS" | "CAMPING" | "CASTLE" | "SKIING"
-  | "CAMPERS" | "ARTIC" | "BOAT" | "BED_AND_BREAKFASTS" | "ROOMS" | "EARTH_HOMES" | "TOWER" | "CAVES"
-  | "LUXES" | "CHEFS_KITCHEN"
+export type CategoryName =
+  "ALL" |
+  "AMAZING_VIEWS" |
+  "OMG" |
+  "TREEHOUSES" |
+  "BEACH" |
+  "FARMS" |
+  "TINY_HOMES" |
+  "LAKE" |
+  "CONTAINERS" |
+  "CAMPING" |
+  "CASTLE" |
+  "SKIING" |
+  "CAMPERS" |
+  "ARTIC" |
+  "BOAT" |
+  "BED_AND_BREAKFASTS" |
+  "ROOMS" |
+  "EARTH_HOMES" |
+  "TOWER" |
+  "CAVES" |
+  "LUXES" |
+  "CHEFS_KITCHEN";
+
 
 export interface Category {
   icon: IconName,
