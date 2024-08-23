f44fc8f187d89e5eac10edcb73a8f4ecb25f67aa
phuonghieuto <phuonghieuto@gmail.com>
2024-08-23 15:19:08 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/phuonghieuto/airbnb-frontend/commit/8f01e6d64afec0a49e765a35e066ca3f3e2d6bcb

--- src/environments/environment.production.ts
+++
@@ -1,9 +0,0 @@
-const BACKEND_URL = "https://airbnb-backend-1rhy.onrender.com";
-
-export const environment = {
-  BACKEND_URL: BACKEND_URL,
-  API_URL: `${BACKEND_URL}/api`,
-  AUTH0_CLIENT_ID: "kDprkBlutGRFrNdhNTy7dWNhT3s7VPy7",
-  AUTH0_DOMAIN: "https://dev-q8nycy7azu87tv6h.jp.auth0.com",
-  FRONTEND_URL: "https://phuonghieuto.github.io/airbnb-frontend",
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-const BACKEND_URL = "http://localhost:8080";
-
-export const environment = {
-  BACKEND_URL: BACKEND_URL,
-  API_URL: `${BACKEND_URL}/api`,
-  AUTH0_CLIENT_ID: "kDprkBlutGRFrNdhNTy7dWNhT3s7VPy7",
-  AUTH0_DOMAIN: "https://dev-q8nycy7azu87tv6h.jp.auth0.com",
-  FRONTEND_URL: "http:localhost:4200",
-};
